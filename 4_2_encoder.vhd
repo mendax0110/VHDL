d58a09d392d945e94283e3a0000e94099ff4cf6a
m3ndax <adrian.goessl@outlook.com>
2022-05-31 20:30:18 +0200

    rewrite

--- 4_2_encoder.vhd
+++ 4_2_encoder.vhd
@@ -1,25 +1,24 @@
------------------------------------------------------
----------------------4:2 Encoder---------------------
------------------------------------------------------
+-------------------------------------------------------------
+---------------------4:2 Encoder in VHDL---------------------
+-------------------------------------------------------------
 library IEEE;
-use IEEE.std_logic_1164.all;
+use IEEE.STD_LOGIC_1164.ALL;
 
---entitiy declaration--
+--entity declaration--
 entity four_two_encoder is
     port(   --Inputs--
-            a : in std_logic_vector(3 downto 0);
+            a   : in std_logic_vector(3 downto 0);
             --Outputs--
-            y : out std_logic);
+            y   : out std_logic);
 end entity four_two_encoder;
 
---architecture declaraion--
+--architecture declaration--
 architecture four_two_encoder_arch of four_two_encoder is
 
     begin
         y <= "00" when a = "0001" else
-             "01" when a = "0010" else 
+             "01" when a = "0010" else
              "10" when a = "0100" else
              "11" when a = "1000" 
 
 end architecture four_two_encoder_arch;
-
