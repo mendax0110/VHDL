446b33f6f2555f1641852374caf9e8b96ee79d46
m3ndax <adrian.goessl@outlook.com>
2022-06-04 00:21:02 +0200

    rewrite some code

--- clk_bypass.vhd
+++ clk_bypass.vhd
@@ -1,18 +1,17 @@
--------------------------------------------------------
----------------clock bypass in vhdl--------------------
--------------------------------------------------------
+------------------------------------------------------------
+--------------------CLOCK BYPASS IN VHDL--------------------
+------------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
 
---entity declarations--
+--entity delcarations--
 entity clock_generator is
     generic(
-        CLK_INPUT_HZ    : positive := 50000000;
-        CLK_OUTPUT_HZ   : positive := 50000000;
+        CLK_INPUT_HZ    : integer := 50000000;
+        CLK_OUTPUT_HZ   : integer := 50000000;
     );
-
     port(   --Inputs--
-            ext_clk         : in std_logic;
+            ext_clk_i       : in std_logic;
             pll_rst_in      : in std_logic;
             --Outputs--
             pll_clk_out     : out std_logic;
@@ -25,7 +24,7 @@ architecture bypass of clock_generator is
     begin
         assert CLK_INPUT_HZ <= CLK_OUTPUT_HZ severity FAILURE;
 
-        pll_locked_out <= pll_rst_in;
-        pll_clk_out    <= ext_clk;
+        pll_locked_out  <= pll_rst_in;
+        pll_clk_out     <= ext_clk;
 
 end architecture bypass;
