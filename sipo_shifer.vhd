efc2eb1f87b588e6420821801ec63d95d723b8c3
m3ndax <adrian.goessl@outlook.com>
2022-06-11 11:44:32 +0200

    update comment and code

--- sipo_shifer.vhd
+++ sipo_shifer.vhd
@@ -1,21 +1,28 @@
+-------------------------------------------------------------
+--------------------SIPO-SHIFTER IN VHDL---------------------
+-------------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity sipo is
-    port(   clk, clear : in std_logic;
-            Input_Data : in std_logic;
-            Q          : out std_logic_vector(3 downto 0));
-end sipo;
+--entity declarations--
+entity sipo_shifter is
+    port(   --Inputs--
+            clk, clear  : in std_logic;
+            Input_Data  : in std_logic;
+            --Outputs--
+            Q           : out std_logic_vector(3 downto 0));
+end entity sipo_shifter;
 
-architecture arch of sipo is
+--architecure definitions--
+architecture sipo_shifter_arch of sipo_shifter is
     begin
         process(clk)
             begin
                 if clear = '1' then
                     Q <= "0000";
-                elsif (CLK'event and CLK = '1') then
+                elsif(CLK'event and CLK = '1') then
                     Q(3 downto 1) <= Q(2 downto 0);
                     Q(0) <= Input_Data;
                 end if;
         end process;
-end arch;
+end architecture sipo_shifter_arch;
