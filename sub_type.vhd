34c5fc49c0561c7224b557c80b13e01a8573e8f0
m3ndax <adrian.goessl@outlook.com>
2022-06-17 17:20:20 +0200

    update code and comment

--- sub_type.vhd
+++ sub_type.vhd
@@ -1,26 +1,36 @@
+-----------------------------------------------------------
+-----------------------SUB-TYPE IN VHDL--------------------
+-----------------------------------------------------------
 package mux_type is
-    subtype eightval is integer range 0 to 7; -- line 1
-end mux_type;
+    subtype eightval is integer range 0 to 7; -- 8-bit value
+end package mux_type;
 
-use work.mux_type.all;
+--include the libraries--
 library IEEE;
-use IEEE.std_logic_1164.all;
+use IEE.std_logic_1164.all;
+use work.mux_type.all;
 
+--entity declarations--
 entity mux8 is
-    port(I0, I1, I2, I3, I4, I5, I6, I7 : in std_logic;
-                                    sel : in eightval; --line 2
-                                    q   : out std_logic);
-end mux8;
+    port(   --Inputs--
+            I0, I1, I2, I3, I4, I5, I6, I7  : in std_logic;
+            sel                             : in eightval; --selector
+            --Outputs--
+            q                               : out std_logic);
+end entity mux8;
 
-architecture mux8 of mux8 is
+--architecure definitions--
+architecture mux8_arch of mux8 is
+
     begin
-        with sel select     -- line 3
-            Q <= I0 after 10 ns when 0,  --line 4
-                 I1 after 10 ns when 1,  --line 5
-                 I2 after 10 ns when 2,  --line 6
-                 I3 after 10 ns when 3,  --line 7
-                 I4 after 10 ns when 4,  --line 8
-                 I5 after 10 ns when 5,  --line 9
-                 I6 after 10 ns when 6,  --line 10
-                 I7 after 10 ns when 7,  --line 11
-end mux8;
+        with sel select 
+            Q <=    I0 after 10 ns when 0,  --selector = 0
+                    I1 after 10 ns when 1,  --selector = 1 
+                    I2 after 10 ns when 2,  --selector = 2
+                    I3 after 10 ns when 3,  --selector = 3
+                    I4 after 10 ns when 4,  --selector = 4
+                    I5 after 10 ns when 5,  --selector = 5
+                    I6 after 10 ns when 6,  --selector = 6
+                    I7 after 10 ns when 7;  --selector = 7
+
+end architecture mux8_arch;
