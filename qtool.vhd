95edf0faaf15e02f8ccfba2f7f05c23fbb8b6f22
m3ndax <adrian.goessl@outlook.com>
2022-06-18 20:17:00 +0200

    update code and comment

--- qtool.vhd
+++ qtool.vhd
@@ -1,5 +1,5 @@
 --------------------------------------------------------
-----------------Q Tool in vhdl--------------------------
+---------------------Q-TOOL IN VHDL---------------------
 --------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
@@ -12,16 +12,16 @@ package q_tool is
 
 end package q_tool;
 
+--architecure definitions--
 architecture q_tool_arch of q_tool is
 
     function f_log2(x: positive) return natural is
-        variable i : natural;
+        variable i: natural := 0;
         begin
             i := 0;
             while(2**i < x) and i < 127 loop
                 i := i + 1;
             end loop;
-            return i;
-    end function
+    end function f_log2;
 
 end architecture q_tool_arch;
