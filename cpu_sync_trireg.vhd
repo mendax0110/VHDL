f45e33e402b9a726db4502765698b224f2f23d14
m3ndax <adrian.goessl@outlook.com>
2022-06-25 12:05:15 +0200

    update code and comment

--- cpu_sync_trireg.vhd
+++ cpu_sync_trireg.vhd
@@ -1,34 +1,41 @@
+----------------------------------------------
+------------CPU-SYNC-TRIEG IN VHDL------------
+----------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
 use work.cpu_lib.all;
 
+--entity declaraions--
 entity trireg is
-    port(   a   : in bit16;
+    port(   --Inputs--
+            a   : in bit16;
             en  : in std_logic;
             clk : in std_logic;
+            --Outputs--
             q   : out bit16);
-end trireg;
+end entity trireg;
 
-architecture rtl of trireg is
+--architecture definitions--
+architecture trireg_arch of trireg is
+
     signal val : bit16;
+
     begin
         triregdata : process
         begin
             wait until clk'event and clk = '1';
             val <= a;
-        end process;
+        end process triregdata;
 
         trireg3st : process(en, val)
         begin
-            if en = '1' then
+            if(en = '1') then
                 q <= val after 1 ns;
-            elsif en = '0' then
+            elsif(en = '0') then
                 q <= "ZZZZZZZZZZZZZZZZ" after 1 ns;
-        -- exemplar_translate_off--
             else
                 q <= "XXXXXXXXXXXXXXXX" after 1 ns;
-        -- exemplar_translate_on--
             end if;
-        end process;
-end rtl;
-        
+        end process trireg3st;
+
+end architecture trireg_arch;
