3436d1dbb0c5b26c95725c8f19c2a6c3a3ff0cf1
m3ndax <adrian.goessl@outlook.com>
2022-06-28 19:56:36 +0200

    update code and comment

--- sck.vhd
+++ sck.vhd
@@ -1,47 +1,50 @@
-------------------------------------------------------------------------------
-----------------------Serial Clock--------------------------------------------
-------------------------------------------------------------------------------
+------------------------------------------------------------
+--------------------SERIAL-CLOCK IN VHDL--------------------
+------------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
 
 --entity declarations--
 entity sck is
     port(   --Inputs--
-            reset       : in std_logic;
-            clk_in      : in std_logic;
-            dat_in      : in std_logic;
+            reset   : in std_logic;
+            clk_in  : in std_logic;
+            dat_in  : in std_logic;
             --Outputs--
-            dat_out     : out std_logic);
+            dat_out : out std_logic);
 end entity sck;
 
---architecture definitions--
+--architecture declarations--
 architecture sck_arch of sck is
 
+    --signal declarations--
     signal clk  : std_logic;
 
-    component BUFG
-        port(   --Inputs--
+    --component declarations--
+    component BUFG is
+        port(   --Inputs--   
                 I   : in std_logic;
                 --Outputs--
                 O   : out std_logic);
     end component BUFG;
 
     begin
-        ----------------------Design Implementation-------------------------
+        ---------------Design Implementation---------------
 
         --serial clock buffer instantiation--
         clock_buf : BUFG
-            port map(   
-                        I   => clk_in,
-                        O   => clk);
+            port map(
+                I   => clk_in,
+                O   => clk
+            );
 
-        Reg_Proc: process(clk)
+        Reg_Proc : process(clk)
         begin
             if(reset = '1') then
                 dat_out <= '0';
             elsif rising_edge(clk) then
                 dat_out <= dat_in;
             end if;
-        end process;
+        end process Reg_Proc;
 
 end architecture sck_arch;
