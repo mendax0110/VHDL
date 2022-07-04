a9084f13eccb1b51d4eb514bf93c6b41fe3d9b8a
m3ndax <adrian.goessl@outlook.com>
2022-07-04 21:51:43 +0200

    update code and comment

--- global_signal.vhd
+++ global_signal.vhd
@@ -1,26 +1,45 @@
+-----------------------------------------------------
+---------------GLOBAL-SIGNAL IN VHDL-----------------
+-----------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity and5 is
-    port(   a, b, c, d, e : in std_logic;
-                        q : out std_logic);
-end and5;
+--entity definitions--
+entity global_sig is
+    port(   --Inputs--
+            a   : in std_logic;
+            b   : in std_logic;
+            c   : in std_logic;
+            d   : in std_logic;
+            e   : in std_logic;
+            --Outputs--
+            q   : out std_logic);
+end entity global_sig;
 
-architecture and5 of and5 is
+--architecture definitions--
+architecture global_sig_arch of global_sig is
+
     begin
+        --process--
         process(a, b, c, d, e)
+            --variables--
             variable state : std_logic;
             variable delay : time;
+            
+            --statements--
             begin
-                state := a and b and c and d and e;
+                state <= a and b and c and d and e;
+                --delay--
                 if state = '1' then
-                    delay := 4,5 ns;
+                    delay <= 4,5 ns;
                 elsif state = '0' then
-                    delay := 3 ns;
+                    delay <= 3 ns;
                 else
-                    delay := 4 ns;
+                    delay <= 4 ns;
                 end if;
 
-                q <= state after delay
+                q <= state after delay;
         end process;
-end and5;
+
+end architecture global_sig_arch;
+
