4b439eee8f34b96fddb88930d4eb24a9c994e529
m3ndax <adrian.goessl@outlook.com>
2022-07-09 13:38:38 +0200

    update code and comment

--- incrementer.vhd
+++ incrementer.vhd
@@ -1,28 +1,39 @@
+-------------------------------------------------------
+------------------INCREMENTER IN VHDL------------------
+-------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
 
+--entity declarations--
 entity incrementer is
-    generic (width : integer :=8);
-    port ( datain : in std_logic_vector(width-1 downto 0);
-           control: in std_logic;
-           dataout: out std_logic_vector(width-1 downto 0);
-           flag : out std_logic);
-end incrementer;
+    --generic declarations--
+    generic(width : integer := 8);
+    --port declarations--
+    port(   --Inputs--
+            datain      : in std_logic_vector(width-1 downto 0);
+            control     : in std_logic;
+            --Outputs--
+            dataout     : out std_logic_vector(width-1 downto 0)
+            flag        : out std_logic);
+end entity incrementer;
 
-architecture behv of incrementer is
+--architecture definitions--
+architecture increment_arch of incrementer is
 
-signal dataout_int: std_logic_vector(width-1 downto 0);
-begin
-    process(datain, control)
     begin
-        if (control = '1')then --increment
-            dataout_int<=datain + '1';
-        else --feedthrough
-            dataout_int<=datain;
-        end if;
-    end process;
-    dataout<=dataout_int;
-    flag<='1'when(control= '1' and datain = To_std_logic_vector(X"FF")) else '0';
-end behv;
+        --prossessing--
+        process(datain, control)
+        begin
+            if(control = '1') then --increment--
+                dataout_int <= datain + '1';
+            else --feedthrough--
+                dataout_int <= datain;
+            end if;
+        end process;
 
+        --output assignments--
+        dataout <= dataout_int;
+        flag <= '1' when (control = '1' and datain = To_std_logic_vector(X"FF")) else '0';
+
+end increment_arch;
