84f8ea531ad0b4e952dcf0ce49540f8a9422c7d3
m3ndax <adrian.goessl@outlook.com>
2022-07-18 13:18:21 +0200

    update code and comment

--- 4bit_arith.vhd
+++ 4bit_arith.vhd
@@ -1,26 +1,33 @@
+--------------------------------------------------------------
+---------------4-BIT-ARITHMETIC-COUNTER IN VHDL---------------
+--------------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
+--entity declarations--
 entity four_bit_counter is
-    port(   clk     : in std_logic;
+    port(   --Inputs--
+            clk     : in std_logic;
             reset   : in std_logic;
+            --Outputs--
             counter : out std_logic_vector(3 downto 0));
-end four_bit_counter;
+end entity four_bit_counter;
 
---architecture of entitiy--
-architecture structural of four_bit_counter is
+--architecture declarations--
+architecture four_bit_counter_arch of four_bit_counter is
 
-    --signal declaration--
+    --signals declarations--
     signal q_int : unsinged(3 downto 0);
         begin
             counter <= std_logic_vector(q_int);
             process(clk, reset)
-                    begin
-                        if reset = '1' then --reset auf high
-                            q_int <= (others => '0'); --zählerstand zurücksetzen
-                        elsif rising_edge(clk) then -- steigende flanke an clk
-                            q_int <= q_int + 1; --zählerstand um 1 erhöhen
-                        end if;
+            begin
+                if reset = '1' then                 --reset on high--
+                    q_int <= (others => '0');       --counter reset--
+                elsif clk'event and clk = '1' then  --clock rising edge--
+                    q_int <= q_int + 1;             --counter increment--
+                end if;
             end process;
-end structural;
+
+end architecture four_bit_counter_arch;
