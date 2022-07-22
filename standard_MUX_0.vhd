7fd76342cbad5278901ac1ee17404e158989f104
m3ndax <adrian.goessl@outlook.com>
2022-07-22 17:29:07 +0200

    update code and comments

--- standard_MUX_0.vhd
+++ standard_MUX_0.vhd
@@ -1,21 +1,27 @@
+----------------------------------------------------------
+---------------STANDARD-MULTIPLEXER IN VHDL---------------
+----------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
 
---entity declaration--
-entity standard_mux is
-    port(   --Inputs--
-            in_1 : in std_logic_vector(3 downto o);
-            in_2 : in std_logic_vector(3 downto 0);
-            in_3 : in std_logic_vector;
+--entity declarations--
+entity standard_mux_0 is
+    port(   --Inputs-
+            in_1    : in std_logic_vector(3 downto 0);
+            in_2    : in std_logic_vector(3 downto 0);
+            in_3    : in std_logic;
             --Outputs--
-            out_1 : out std_logic_vector(3 downto 0));
-end entity standard_mux;
+            out_1   : out std_logic_vector(3 downto 0));
+end entity standard_mux_0;
 
---architecture declaration--
+--architecture declarations--
 architecture standard_mux_0_arch of standard_mux_0 is
+
     begin
-        with (in_3) select
-            out_1 <= in_1 when '0',
-                     in_2 when '1',
-                     "0000" when others;
+        --Processing--
+        with(in_3) select
+            out_1   <=  in_1 when '0',
+                        in_2 when '1',
+                        "0000" when others;
+
 end architecture standard_mux_0_arch;
