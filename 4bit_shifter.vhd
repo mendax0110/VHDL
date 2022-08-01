c77e13dfb77a92cd0076aa47c0e9bba937443443
m3ndax <adrian.goessl@outlook.com>
2022-08-01 21:50:00 +0200

    update code and comment

--- 4bit_shifter.vhd
+++ 4bit_shifter.vhd
@@ -1,39 +1,54 @@
+-------------------------------------------------------------------
+-----------------------4-BIT-SHIFTER IN VHDL-----------------------
+-------------------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-package shift_type is
+--package declaration--
+package shifter_type is
     subtype bit4 is std_logic_vector(3 downto 0);
-end shift_type;
+end package shifter_type;
 
-use work.shift_types.all;
 library IEEE;
+use work.shifter_types.all;
 use IEEE.std_logic_1164.all;
 
+--entity declaration--
 entity shifter is
-    port(din                   : in bit4;
-         clk, load, left_right : in std_logic;
-         dout                  : inout bit4);
-end shifter;
+    port(   --Inputs--
+            din         : in bit4;
+            clk         : in std_logic;
+            load        : in std_logic;
+            left_right  : in std_logic;
+            --Outputs--
+            dout        : out bit4);
+end entity shifter;
 
-architecture synth of shifter is
+--architecture declaration--
+architecture shifter_type_arch of shifter_type is
+
+    --signal declaration--
     signal shift_val : bit4;
         begin
-        nxt : process(load, left_right, din, dout)
-            begin
-                if(load = '1') then
-                    shift_val <= din;
-                elsif(left_right = '0') then
-                    shift_val(2 downto 0) <= dout(3 downto 1);
-                    shift_val(3) <= '0';
-                else
-                    shift_val(3 downto 1) <= dout(2 downto 0);
-                    shift_val(0) <= '0';
-                end if;
-        end process;
+            --process declaration--
+            nxt : process(load, left_right, din, dout)
+                begin
+                    if(load = '1') then
+                        shift_val <= din;
+                    elsif(left_right = '0') then
+                        shift_val(3 downto 0) <= dout(3 downto 1);
+                        shift_val(3) <= '0';
+                    else
+                        shift_val(3 downto 1) <= dout(2 downto 0);
+                        shift_val(0) <= '0';
+                    end if;
+            end process;
 
-        current : process
-            begin
-                wait until clk'EVENT and clk = '1';
-                dout <= shift_val;
-        end process;
-end synth;
+            --process declaration--
+            current : process
+                begin
+                    wait until clk'EVENT and clk = '1';
+                    dout <= shift_val;
+            end process;
+
+end architecture shifter_type_arch;
