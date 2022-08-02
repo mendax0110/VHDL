739dc1fb80af3b8b8e081d81e7eacef00d53d054
m3ndax <adrian.goessl@outlook.com>
2022-08-02 16:40:50 +0200

    update code and comment

--- 4bit_register.vhd
+++ 4bit_register.vhd
@@ -1,51 +1,70 @@
+----------------------------------------------------------------------
+------------------------4-BIT-REGISTER IN VHDL------------------------
+----------------------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity reg_test is 
-    port(   clk : in std_logic;
-            ce  : in std_logic;
-            clr : in std_logic;
-            d   : in std_logic_vector(3 downto 0);
-            q   : out std_logic_vector(3 downto 0));
-end reg_test;
+--entity declarations--
+entity reg_test is
+    port(   --Inputs--
+            clk     : in std_logic;
+            ce      : in std_logic;
+            clr     : in std_logic;
+            d       : in std_logic_vector(3 downto 0);
+            --Outputs--
+            q       : out std_logic_vector(3 downto 0));
+end entity reg_test;
 
-architecture regtest_arch of reg_test is
+--architecture declarations--
+architecture reg_test_arch of reg_test is
+
+    --component declarations--
     component FDCE_A
-        port(   clk : in std_logic;
+        port(   --Inputs--
+                clk : in std_logic;
                 ce  : in std_logic;
                 clr : in std_logic;
                 d   : in std_logic;
+                --Outputs--
                 q   : out std_logic);
-    end component;
+    end component FDCE_A;
 
         begin
-            --component instances--
+            --component instantiations--
             fdc_1a : FDCE_A
-            port map(   d   => d(0),
-                        clk => clk,
-                        ce  => ce,
-                        clr => clr,
-                        q   => q(0));
+            port map(
+                d   => d(0),
+                clk => clk,
+                ce  => ce,
+                clr => clr,
+                q   => q(0)
+            );
 
             fdc_2a : FDCE_A
-            port map(   d   => d(1),
-                        clk => clk,
-                        ce  => ce,
-                        clr => clr,
-                        q   => q(1)); 
-            
-            fdc_3a : FDCE_A
-            port map(   d   => d(2),
-                        clk => clk,
-                        ce  => ce,
-                        clr => clr,
-                        q   => q(2));
-            
-            fdc_4a : FDCE_A
-            port map(   d   => d(3),
-                        clk => clk,
-                        ce  => ce,
-                        clr => clr,
-                        q   => q(3));
+            port map(
+                d   => d(1),
+                clk => clk,
+                ce  => ce,
+                clr => clr,
+                q   => q(1)
+            );
 
-end regtest_arch;
+            fdc_3a : FDCE_A
+            port map(
+                d   => d(2),
+                clk => clk,
+                ce  => ce,
+                clr => clr,
+                q   => q(2)
+            );
+
+            fdc_4a : FDCE_A
+            port map(
+                d   => d(3),
+                clk => clk,
+                ce  => ce,
+                clr => clr,
+                q   => q(3)
+            );
+            
+end architecture reg_test_arch;
