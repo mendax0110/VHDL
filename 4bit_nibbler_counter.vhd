a1c506a1025cdd0c7d1a10afad601390bb5a6412
m3ndax <adrian.goessl@outlook.com>
2022-08-06 13:31:19 +0200

    update code and comment

--- 4bit_nibbler_counter.vhd
+++ 4bit_nibbler_counter.vhd
@@ -1,29 +1,39 @@
+-----------------------------------------------------------------------------------
+---------------------------4-BIT-NIBBLER-COUNTER IN VHDL---------------------------
+-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
 
+--entity declaration--
 entity up_counter is
-    port(   clk     : in std_logic;
-            reset   : in std_logic;
+    port(   --Inputs-- 
+            clk     : in std_logic;
+            rst     : in std_logic;
+            --Outputs--
             counter : out std_logic_vector(3 downto 0));
-end up_counter;
+end entity up_counter;
 
-architecture Behavioral of up_counter is 
-    
+--architecture definition--
+architecture up_counter_arch of up_counter is
+
+    --signal declaration--
     signal counter_up : std_logic_vector(3 downto 0);
 
         begin
+            --process declaration--
             process(clk)
                 begin
                     if(rising_edge(clk)) then
-                        if(reset = '1') then
-                            counter_up <= x "0";
+                        if(rst = '1') then
+                            counter_up <= (others => '0');
                         else
-                            counter_up <= counter_up + X "1";
+                            counter_up <= counter_up + 1;
                         end if;
                     end if;
             end process;
-            
-            counter <= counter_up;
-end behavioral;
 
+            --output assignment--
+            counter <= counter_up;
+
+end architecture up_counter_arch;
