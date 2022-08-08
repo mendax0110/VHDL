add080ef9cbda466768500178d89bfd06120580a
m3ndax <adrian.goessl@outlook.com>
2022-08-08 12:50:39 +0200

    update code and comment

--- 4bit_nibbler_divider.vhd
+++ 4bit_nibbler_divider.vhd
@@ -1,29 +1,42 @@
+---------------------------------------------------------------------------
+-----------------------4-BIT-NIBBLER-DIVIDER IN VHDL-----------------------
+---------------------------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
 
+--entity declarations--
 entity clock_divider is
-    generic( T_FAKTOR : interger := 50 * 10 ** 6 / 5);
-    port (  clk    : in std_logic;
-            reset  : in std_logic;
-            enable : out std_logic);
-end clock_divider;
+    --generic declarations--
+    generic(T_FAKTOR : integer := 50 * 10 ** 6 / 5);
+    --port declarations--
+    port(   --inputs--
+            clk_in  : in std_logic;
+            reset   : in std_logic;
+            --outputs--
+            enable  : out std_logic);
+end entity clock_divider;
 
-architecture behavioral of clock_divider is
+--architecture declarations--
+architecture clock_divider_arch of clock_divider is
+
+    --signal declarations--
     signal cnt : integer;
+
         begin
-            ClockDiv : process(reset,clk)
+            ClockDiv : process(reset, clk)
                 begin
                     if reset = '1' then
                         cnt <= T_FAKTOR -1;
                         enable <= '0';
                     elsif clk = '1' and clk'event then
                         if cnt = 0 then
-                            enable <= '1';
                             cnt <= T_FAKTOR -1;
+                            enable <= '1';
                         else
+                            cnt <= cnt - 1;
                             enable <= '0';
-                            cnt <= cnt -1;
                         end if;
                     end if;
             end process ClockDiv;
-end behavioral;
+        
+end architecture clock_divider_arch;
