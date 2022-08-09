25cb52b3245348ef314428bb54e42e38ce341bd2
m3ndax <adrian.goessl@outlook.com>
2022-08-09 15:23:33 +0200

    update code and comment

--- 4bit_nibbler_rom.vhd
+++ 4bit_nibbler_rom.vhd
@@ -1,45 +1,65 @@
+---------------------------------------------------------------------------------------
+-------------------------------4-BIT-NIBBLER-ROM IN VHDL-------------------------------
+---------------------------------------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
 
+--entity declarations--
 entity rom is
-    port(   address : in std_logic_vector(7 downto 0);
-            opcode  : out std_logic_vector(7 downto 0) := (others => '1');
-            clk     : in std_logic);
-end rom;
+    port(   --Inputs--
+            address : in std_logic_vector(7 downto 0);
+            clk     : in std_logic;
+            --Outputs--
+            opcode  : out std_logic_vector(7 downto 0) := (others => '1'));
+end entity rom;
 
-architecture behaviour of rom is
-    
-    signal myufm_addr, myufm_dataout : std_logic_vector(7 downto 0);
-    signal myufm_nread, myufm_nbusy, myufm_data_valid : std_logic;
+--architecture declarations--
+architecture rom_arch of rom is
 
-        component ufm_parallel
-            port(   ufm_addr_addr               : in std_logic_vector(7 downto 0) := (others => '0');
-                    ufm_data_vaild_data_valid   : out std_logic;
-                    ufm_dataout_dataout         : out std_logic_vector(7 downto 0);
-                    ufm_nbusy_nbusy             : out std_logic;
-                    ufm_nbusy_nread             : in std_logic := '0');
-        end component;
+    --signals declarations--
+    signal myufm_addr, myufm_dataout                    : std_logic_vector(7 downto 0);
+    signal myufm_nread, myufm_nbusy, myufm_data_valid   : std_logic;
 
-        shared variable nextopcode : std_logic_vector(7 downto 0) := (others => '1');
+    --component declarations--
+    component ufm_parallel
+        port(   --Inputs--
+                ufm_addr_addr       : in std_logic_vector(7 downto 0) := (others => '0');
+                ufm_nbusy_nread     : in std_logic := '0';
+                --Outputs--
+                ufm_datatout        : out std_logic_vector(7 downto 0);
+                ufm_data_vaild      : out std_logic;
+                ufm_nbusy           : out std_logic := '0');
+    end component;
+
+    shared variable nextopcode : std_logic_vector(7 downto 0) := (others => '1');
 
         begin
-            
-            ufm : ufm_parallel port map(myufm_addr, myufm_data_valid, myufm_dataout, myufm_nbusy, myufm_nread);
 
+            --component instantiations--
+            ufm : ufm_parallel
+                port map(
+                    ufm_addr            => myufm_addr,
+                    ufm_nbusy           => myufm_nread,
+                    ufm_datatout        => myufm_dataout,
+                    ufm_data_vaild      => myufm_data_valid,
+                    ufm_nbusy           => myufm_nbusy);
+
+            --process to clock the ROM--
             process(clk)
                 begin
                     if rising_edge(clk) then
-                            opcode <= nextopcode;
-                            myufm_addr <= addres;
-                            myufm_nread <= '0';
+                        opcode      <= nextopcode;
+                        myufm_addr  <= address;
+                        myufm_nread <= '0';
                     end if;
             end process;
-
+            
+            --process to read the opcode from the ROM--
             process(myufm_data_valid)
                 begin
                     if rising_edge(myufm_data_valid) then
-                            nextopcode := myufm_dataout;
+                        nextopcode := myufm_dataout;
                     end if;
-             end process;
+            end process;
 
-end behaviour;
+end architecture rom_arch;
