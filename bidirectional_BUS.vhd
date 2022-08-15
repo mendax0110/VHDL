e7e8505c569caee201302558a9c3d57a59bf5b37
m3ndax <adrian.goessl@outlook.com>
2022-08-15 11:52:00 +0200

    update code and comment

--- bidirectional_BUS.vhd
+++ bidirectional_BUS.vhd
@@ -1,19 +1,29 @@
+---------------------------------------------------------------------------------
+----------------------------BIDIRECTIONAL-BUS IN VHDL----------------------------
+---------------------------------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
 
+--entity declaration--
 entity bidir is
-    port(   bidir   : inout std_logic_vector(7 downto 0);
-            oe, clk : in std_logic;
+    port(   --Inputs--
+            bidir   : in std_logic_vector(7 downto 0);
+            oe      : in std_logic;
+            clk     : in std_logic;
             inp     : in std_logic_vector(7 downto 0);
-            outp    : out std_logic_vector(7 downto 0));
-end bidir;
+            --Outputs--
+            out     : out std_logic_vector(7 downto 0));
+end entity bidir;
 
+--architecture declaration--
 architecture maxpld of bidir is
 
+    --Signals declaration--
     signal a : std_logic_vector(7 downto 0);
     signal b : std_logic_vector(7 downto 0);
 
         begin
+            --process declaration--
             process(clk)
                 begin
                     if clk = '1' and clk'EVENT then
@@ -24,12 +34,13 @@ architecture maxpld of bidir is
 
             process(oe, bidir)
                 begin
-                    if (oe = '0') then
-                        bidir <= "ZZZZZZZZ"
-                        b <= bidir;
+                    if(oe = '1') then
+                        bdir <= "ZZZZZZZZ"
+                        b <= bdir;
                     else
-                        bidir <= a;
-                        b <= bidir;
+                        bdir <= a;
+                        b <= bdir;
                     end if;
             end process;
-end maxpld;
+
+end architecture maxpld;
