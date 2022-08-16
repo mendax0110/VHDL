2779c7b17a8552e08ca31953b93b830ffc298e0a
m3ndax <adrian.goessl@outlook.com>
2022-08-16 16:11:45 +0200

    update code and comment

--- clockdivider_4bit_nibbler.vhd
+++ clockdivider_4bit_nibbler.vhd
@@ -1,32 +1,44 @@
+---------------------------------------------------------------
+---------------CLOCKDIVIDER-4BIT-NIBBLER IN VHDL---------------
+---------------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
+--entity declaration--
 entity clockdivider is
-    port(   clk_in  : in std_logic;
-            clk_out : out std_logic;
-            clk_ena : in std_logic ;= '1');
-end clockdivider;
+    port(   --Inputs--
+            clk_in      : in std_logic;
+            clk_ena     : in std_logic := '1';
+            --Outputs--
+            clk_out     : out std_logic);
+end entity clockdivider;
 
-architecture behaviour of clockdivider is
+--architecture declaration--
+architecture clockdivider_arch of clockdivider is
+
     begin
+        --process declaration--
         process(clk_in)
 
-            variable divider : std_logic_vector(23 downto 0) := (others => '0');
-            variable state   : std_logic := '0';
-
+            --variable declaration--
+            variable divider    : std_logic_vector(23 downto 0) := (others => '0');
+            variable state      : std_logic := '0';
+            
                 begin
                     if rising_edge(clk_in) then
-
                         divider := std_logic_vector(unsigned(divider) + 1);
 
                         if unsigned(divider) = 50000 then
                             divider := (others => '0');
-                            state   := not state:
+                            state   := not state;
+                        
                             if clk_ena = '1' then
-                                    clk_out <= state;
+                                clk_out <= state;
                             end if;
                         end if;
                     end if;
         end process;
-end behaviour;
+
+end architecture clockdivider_arch;
+
