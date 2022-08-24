c42fdc675eaf06572a380fefcc550b05e6535ae3
m3ndax <adrian.goessl@outlook.com>
2022-08-24 17:09:50 +0200

    update code and comment

--- register/pdkreg.vhd
+++ register/pdkreg.vhd
@@ -1,16 +1,18 @@
+--------------------------------------------------------------------
+---------------------------PDKREG IN VHDL---------------------------
+--------------------------------------------------------------------
 library IEEE;
-library work;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
-use work.txt_util.all;
-use work.pdkpkg.all;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use work.txt_util.ALL;
+use work.pdkpkg.ALL;
 
 --entity declaration--
 entity pdkreg is
     generic(
-        RESET : wordtype := (others => '0');
-        NAME  : string;
-        DEBUG_ENABLED : boolean := false
+        RESET         : wordtype := (others => '0');
+        NAME          : string;
+        DEBUG_ENABLED : boolean := false;
     );
     port(   --Inputs--
             clk_i   : in std_ulogic;
@@ -18,30 +20,32 @@ entity pdkreg is
             dat_i   : in wordtype;
             mask_i  : in wordtype;
             wen_i   : in std_logic;
+            --Outputs--
             dat_o   : out wordtype);
 end entity pdkreg;
 
---architecture definition--
+--architecture declaration--
 architecture pdkreg_arch of pdkreg is
 
     --signal definition--
     signal r : wordtype;
 
-    begin
-        process(clk_i, rst_i)
+        --process definition--
         begin
-            if rst_i = '1' then
-                r <= RESET;
-            elsif rising_edge(clk_i) then
-                if wen_i = '1' then
-                    if DEBUG_ENABLED then
-                        report "SFR " & " write, data " & hstr(std_logic_vector(dat_i)) & " mask " & hstr(std_logic_vector(mask_i));
+            process(clk_i, rst_i)
+            begin
+                if(rst_i = '1') then
+                    r <= RESET;
+                elsif(rising_edge(clk_i)) then
+                    if(wen_i = '1') then
+                        if(DEBUG_ENABLED) then
+                            report "SFR" & "write, data" & hstr(std_logic_vector(dat_i)) & "mask" & hstr(std_logic_vector(mask_i));
+                        end if;
+                        r <= (r and not mask_i) or (dat_i and mask_i);
                     end if;
-                    r <= (r and not mask_i) or (dat_i AND mask_i);
                 end if;
-            end if;
-        end process;
+            end process;
 
-        dat_o <= r;
+            dat_o <= r;
 
 end architecture pdkreg_arch;
