2e4315c3b28d058bae526d410fbc1495ad0a08bf
m3ndax <adrian.goessl@outlook.com>
2022-08-26 11:19:27 +0200

    update code and comment

--- fetch_alu.vhd
+++ fetch_alu.vhd
@@ -1,3 +1,6 @@
+-------------------------------------------------------------------------------------------
+--------------------------FETCH-ARITHMETIC-LOGIC-UNIT IN VHDL------------------------------
+-------------------------------------------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
@@ -5,37 +8,46 @@ use IEEE.std_logic_arith.all;
 
 --entity declaration--
 entity fetch is
-    port(   clk         : in std_logic;
+    port(   --Inputs--
+            clk         : in std_logic;
             rst         : in std_logic;
             ctrl        : in std_logic;
             pc_in       : in std_logic_vector(7 downto 0);
+            --Outputs--
             pc_out      : out std_logic_vector(15 downto 0);
             instruction : out std_logic_vector(15 downto 0);
             clk_out     : out std_logic);
-end fetch;
+end entity fetch;
 
---architecture body declaration--
-architecture arch_fetch of fetch is
+--architecture declaration--
+architecture fetch_arch of fetch is
 
+    --signals declaration--
     signal pcout : std_logic_vector(7 downto 0);
 
-    --compnent declaration--
-        component program_counter is
-            port(   clk     : in std_logic;
-                    pc_in   : in std_logic_vector(7 downto 0);
-                    rst_bar : in std_logic; --active low reset signal
-                    pc_ctr1 : in std_logic;
-                    pc_out  : out std_logic_vector(7 downto 0);
-                    clk_out : out std_logic);
-        end component program_counter;
+    --component declaration--
+    component program_counter is
+        port(   --Inputs--
+                clk     : in std_logic;
+                pc_in   : in std_logic_vector(7 downto 0);
+                rst_bar : in std_logic;
+                pc_ctr1 : in std_logic;
+                --Outputs-- 
+                pc_out  : out std_logic_vector(7 downto 0);
+                clk_out : out std_logic);
+    end component program_counter;
 
-        component instruction_memory is
-            port( pc    : in std_logic_vector(7 downto 0);
-            instruction : out std_logic_vector(15 downto 0));
-        end component instruction_memory;
+    --component declaration--
+    component instruction_memory is
+        port(   --Inputs--
+                pc  : in std_logic_vector(7 downto 0);
+                --Outputs--
+                inst : out std_logic_vector(15 downto 0));
+    end component instruction_memory;
 
-    begin --architecture starts here
-    p_c : program_counter port map(clk, pc_in, rst, ctrl, pcout, clk_out);
-    i_m : instruction_memory port map(pcout, instruction);
-    pc_out <= pcout;
-end architecture arch_fetch; --architecture ends here
+    begin
+        p_c : program_counter port map(clk, pc_in, rst, ctrl, pcout, clk_out);
+        i_m : instruction_memory port map(pcout, inst);
+        pc_out <= pcout;
+
+end architecture fetch_arch;
