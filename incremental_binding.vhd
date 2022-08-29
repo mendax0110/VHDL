80a56d838e3b618836bad7402f8dc530b2ed8381
m3ndax <adrian.goessl@outlook.com>
2022-08-29 15:43:45 +0200

    update code and comment

--- incremental_binding.vhd
+++ incremental_binding.vhd
@@ -1,33 +1,58 @@
+-------------------------------------------------------------------
+--------------------INCREMENTAL-BINDING IN VHDL--------------------
+-------------------------------------------------------------------
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+--entity declarations--
 entity dff is
-    generic(delay   : time);
-    port(din, clock : in std_logic;
-              dout  : out std_logic;)
+    generic(delay : time);
+    port(   --Inputs--
+            din     : in std_logic;
+            clock   : in std_logic;
+            --Outputs--
+            dout    : out std_logic);
 end entity dff;
 
+--entity declarations--
 entity top is
-    port(z, clock : in std_logic;
-            qout  : out std_logic);
+    port(   --Inputs--
+            z       : in std_logic;
+            clock   : in std_logic;
+            --Outputs--
+            qout    : out std_logic);
 end entity top;
 
+--architecture declarations--
 architecture struct of top is
+
+    --component declarations--
     component dff is
-        port(d, clk : in std_logic;
+        port(   --Inputs--
+                d   : in std_logic;
+                clk : in std_logic;
+                --Outputs--
                 q   : out std_logic);
     end component dff;
-    for d1 : dff use entity WORK.dff(behave)
-                    generic map(clk_to_q => 5.2 ns)
+
+    for d1 : dff use entity work.dff(behave)
+                    generic map(clk_to_q 0 => 5.2 ns)
                     port map(d => din, clk => clock, q => open);
+
     signal
         begin
-            -- ..
-            --..
-        d1 : dff port map(z, clock, qout);
+            d1 : dff port map(z, clock, qout);
+
 end architecture struct;
 
+--configuration declarations--
 configuration topcon of top is
+    
     for struct
         for d1 : dff generic map(clk_to_1 => 8.1 ns)
-                     port map(q => dout);
+                    port map(q => dout);
         end for;
     end for;
-end configuration topcon;
+
+end configuration;
