a8af2cae319a7dae834e60aadc2d15af18ad5c79
m3ndax <adrian.goessl@outlook.com>
2022-08-30 14:50:55 +0200

    update code and comment

--- pulse_generator/Pulse_Generator_testbench.vhd
+++ pulse_generator/Pulse_Generator_testbench.vhd
@@ -1,33 +1,44 @@
+----------------------------------------------------------------------------
+---------------------PUSLE_GENERATOR_TESTBENCH IN VHDL----------------------
+----------------------------------------------------------------------------
 entity STATE_MACHINE_TB is
-end STATE_MACHINE_TB;
+end entity STATE_MACHINE_TB;
 
 library IEEE;
 use IEEE.std_logic_1164.all;
 
+--architecture definitions--
 architecture ARC_STATE_MACHINE_TB of STATE_MACHINE_TB is
+
+    --component definitions--
     component P_GENERATOR
-        port(   clk     : in std_ulogic;
-                RESET   : in std_ulogic;
+        port(   --Inputs--
+                CLK     : in std_ulogic;
+                RST     : in std_ulogic;
                 TRIG    : in std_ulogic;
+                --Outputs--
                 PULSE   : out std_ulogic);
     end component;
 
-    signal CLK     : std_ulogic;
-    signal RESET   : std_ulogic;
-    signal TRIG    : std_ulogic;
-    signal PULSE   : std_ulogic;
+    signal CLK  : std_ulogic;
+    signal RST  : std_ulogic;
+    signal TRIG : std_ulogic;
+    signal PULSE: std_ulogic;
 
     begin
+
         U1 : P_GENERATOR
-        port map(CLK, RESET, TRIG, PULSE);
+        port map(CLK, RST, TRIG, PULSE);
 
         CREATE_CLOCK : process(CLK)
             begin
-                if clk <= 'U' then clk <= '0' after 1 ns;
-                    else clk <= not clk after 1 ns;
+                if(clk <= 'U') then
+                    clk <= '0' after 1 ns;
+                else
+                    clk <= not clk after 1 ns;
                 end if;
         end process CREATE_CLOCK;
-        
+
         CREATE_PULSE : process(TRIG)
             begin
                 TRIG <= '0' after 10 ns;
@@ -35,17 +46,14 @@ architecture ARC_STATE_MACHINE_TB of STATE_MACHINE_TB is
                         '0' after 20 ns;
         end process CREATE_PULSE;
 
-end ARC_STATE_MACHINE_TB;
+end architecture ARC_STATE_MACHINE_TB;
 
-configuration CFG_STATE_MACHINE of STATE_MACHINE_TB is 
+--configuration definitions--
+configuration CFG_STATE_MACHINE of STATE_MACHINE_TB is
 
     for ARC_STATE_MACHINE_TB
     end for;
 
-end CFG_STATE_MACHINE;
-
-
-
-
+end configuration;
 
 
