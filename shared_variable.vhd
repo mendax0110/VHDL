5178966c9bbedfcc356abc97513e08e9e99131c3
m3ndax <adrian.goessl@outlook.com>
2022-08-31 15:05:43 +0200

    update code and comment

--- shared_variable.vhd
+++ shared_variable.vhd
@@ -1,36 +1,66 @@
+-------------------------------------------------------------------
+----------------------SHARED-VARIABLE-IN VHDL----------------------
+-------------------------------------------------------------------
 package share is
     variable timing_checks_on : BOOLEAN := true;
 end package share;
 
 use WORK.share.all;
+
+--entity declaration--
 entity dff is
-    port(din : in std_logic;
-         clk : in std_logic;
-         q   : out std:std_logic);
+    port(   --Inputs--
+            din     : in std_logic;
+            clk     : in std_logic;
+            --Outputs--
+            q       : out std_logic);
 end entity dff;
 
+--architecure declaration--
 architecture behave of dff is
+
     begin
+        --process declaration--
         process(clk) is
             begin
-                if timing_checks_on then
-                    --timing check statements
+                if(timing_checks_on) then
+                    --timing checks--
+                    if(rising_edge(clk)) then
+                        q <= din;
+                    end if;
                 end if;
-                    --other statements--
         end process;
+
 end architecture behave;
 
 use WORK.share.all;
-entity jkff is
-    port(j, k, clk, se, clr : in std_logic;
-         q, qb,             : out std_logic);
-end jkff;
 
+--entity declaration--
+entity jkff is
+    port(   --Inputs--
+            j   : in std_logic;
+            k   : in std_logic;
+            clk : in std_logic;
+            se  : in std_logic;
+            clr : in std_logic;
+            --Outputs--
+            q   : out std_logic;
+            q   : out std_logic);
+end entity jkff;
+
+--architecure declaration--
 architecture behave of jkff is
+
     begin
+        --process declaration--
         process(clk, set, clr) is
-            if timing_checks_on then
-                --timing check statements--
-            end if;
+            begin
+                if(timing_check_on) then
+                    --timing checks--
+                    if(rising_edge(clk)) then
+                        q <= se;
+                    end if;
+                end if;
         end process;
+    
 end architecture behave;
