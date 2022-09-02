957e3fa8d8c81ed30b6e68b99d080954a800ca98
m3ndax <adrian.goessl@outlook.com>
2022-09-02 14:16:54 +0200

    update code and comment

--- CPU/CPU_top_level.vhd
+++ CPU/CPU_top_level.vhd
@@ -1,34 +1,52 @@
+-----------------------------------------------------------------------
+-------------------------CPU-TOP-LEVEL IN VHDL-------------------------
+-----------------------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
 use work.cpu_lib.all;
 
+--entity cpu_top--
 entity top is
 end top;
 
-architecture behave of top is
+--architecture cpu_top_arch--
+architecture cpu_top_arch of top is
+
+    --component memory--
     component mem
-        port   (addr    : in bit16;
-                sel, rw : in std_logic;
+        port(   --Inputs--
+                addr    : in bit16;
+                sel     : in std_logic;
+                rw      : in std_logic;
+                --Outputs--
                 ready   : out std_logic;
                 data    : inout bit16);
-    end component;
-    
-    component cpu
-        port   (clock, reset, ready : in std_logic;
-                addr                : out bit16;
-                rw, vma             : out std_logic;
-                data                : inout bit 16);
-    end component;
+    end component mem;
 
-    signal addr, data     : bit16;
-    signal vma, rw, ready : std_logic;
-    signal clock, reset   : std_logic := '0';
+    --component cpu--
+    component cpu
+        port(   --Inputs--
+                clk     : in std_logic;
+                rst     : in std_logic;
+                ready   : in std_logic;
+                --Outputs--
+                addr    : out bit16;
+                rw      : out std_logic;
+                vma     : out std_logic;
+                data    : inout bit16);
+    end component cpu;
+
+    --signal declarations--
+    signal addr, data       : bit16;
+    signal vma, rw, ready   : std_logic;
+    signal clk, rst         : std_logic := '0';
 
     begin
 
-        clock <= not clock after 50 ns;
-        reset <= '1', '0' after 100 ns;
+        clk <= not clk after 50 ns;
+        rst <= '1', '0' after 100ns;
 
-        m1 : mem port map (addr, vma, rw, ready, data);
-        u1 : cpu port map (clock, reset, ready, addr, rw, vma, data);
-end behave;
+        m1 : mem port map(addr, vma, rw, ready, data);
+        u1 : cpu port map(clk, rst, read, addr, rw, vma, data);
+
+end architecture cpu_top_arch;
