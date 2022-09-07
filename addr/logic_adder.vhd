3cda16630a1132414458ab1adc2ba59bdb18b0a5
m3ndax <adrian.goessl@outlook.com>
2022-09-07 17:58:58 +0200

    update code and comment

--- addr/logic_adder.vhd
+++ addr/logic_adder.vhd
@@ -1,35 +1,41 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
+-------------------------------------------------------------
+---------------------LOGIC-ADDER IN VHDL---------------------
+-------------------------------------------------------------
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
-entity signed_addr is
-    port
-    (
-        aclr : in   std_logic;
-        clk  : in   std_logic;
-        a    : in   std_logic_vector;
-        b    : in   std_logic_vector;
-        q    : out  std_logic_vector
-    );
-end signed_adder;
+--entity signed_adder--
+entity signed_adder is
+    port(   --Inputs--
+            clr : in std_logic;
+            clk : in std_logic;
+            a   : in std_logic_vector;
+            b   : in std_logic_vector;
+            --Outputs--
+            q   : out std_logic_vector);
+end entity signed_adder;
 
-architecture signed_adder_arch of signed_addr is
-    signal q_s : signed(a'high+1 downto 0); --extra bit wide
+--architecture signed_adder_arch--
+architecture signed_adder_arch of signed_adder is
 
-begin --architecture
-    assert(a'length >= b'length)
-        report "Port A must be the longer vector if different sizes!"
-        severity FAILURE;
-    q <= std_logic_vector(q_s);
+    --signals--
+    signal q_s : signed(a'high+1 downto 0); --extra bit wide--
 
-    adding_proc:
-    process (aclr, clk)
-        begin
-          if (aclr = '1') then
-            q_s <= (others => '0');
-          elsif rising_edge(clk) then
-            q_s <= ('0'&signed(a)) + ('0'&signed(b));
-          end if; --clk'd
-        end process;
+    begin
+        assert(a'length >= b'length)
+            report "Port A must be the longer vector if different sizes!"
+            severity FAILURE;
+        q <= std_logic_vector(q_s);
 
-end signed_adder_arch;
+        adding_proc:
+            process(clk, clr)
+                begin
+                    if(clr = '1') then
+                        q_s <= (others => '0');
+                    elsif(rising_edge(clk)) then
+                        q_s <= ('0' & signed(a)) + ('0' & signed(b));
+                    end if;
+            end process;
+
+end architecture signed_adder_arch;
