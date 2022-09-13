be213f13897312f5aec3dda7073376114ed42388
m3ndax <adrian.goessl@outlook.com>
2022-09-13 13:24:34 +0200

    update code and comment

--- mealy_machine/mealy_machine_testbench.vhd
+++ mealy_machine/mealy_machine_testbench.vhd
@@ -1,44 +1,64 @@
+-------------------------------------------------------------
+---------------MEALY-MACHINE-TESTBENCH IN VHDL---------------
+-------------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
 
+--entity declaration--
 entity TB_MEALY is
-end;
+end TB_MEALY;
 
+--architecture declaration--
 architecture TESTBENCH of TB_MEALY is
 
-        signal CLK  :std_logic;
-        signal X    :std_logic;
-        signal Z    :std_logic;
+    --signal declaration--
+    signal CLK  : std_logic;
+    signal X    : std_logic;
+    signal Z    : std_logic;
 
-        component mealy
-                port(X, CLOCK : int STD_LOGIC;
-                     Z        : out STD_LOGIC);
-        end component;
+    --component declaration--
+    component mealy
+        port(   --Inputs--
+                CLK : in std_logic;
+                X   : in std_logic;
+                --Outputs--
+                Z   : out std_logic
+            );
+    end component;
 
     begin
+        --process declaration--
         UUT : mealy
-                port map(X, CLK, Z);
-    --CLOCK STIMULI OF 100 NS TIME PERIOD
+            port map(  --Inputs--
+                        CLK => CLK,
+                        X   => X,
+                        --Outputs--
+                        Z   => Z
+                    );
+            
+        --clock stimuli of 100 NS TIME PERIOD----
         CLOCK : process
         begin
-            CLK <= '0', '1' after 50 ns;
-                        wait for 100 ns;
+            CLK <= '0';
+            wait for 50 ns;
+            CLK <= '1';
+            wait for 50 ns;
         end process;
-    
-    --X input STIMULI
+
+        --X input stimuli--
         X_Stimuli : process
         begin
             x <= '0', '1' after 30 ns;
                       'U' after 60 ns;
-                       wait for 90 ns;
+                      wait for  90 ns;
         end process;
-end TESTBENCH;
 
-configuration CFG_TB_MEALY of TB_MEALY is
+end architecture;
+
+--configuration declaration--
+configuration TB_MEALY of TB_MEALY is
     for TESTBENCH
-        for UUT : MEALY
+            for UUT : MEALY
         end for;
     end for;
-end;
-
-
+end configuration;
