045af7a377425ac842f0e0e3900ac75cd963b752
m3ndax <adrian.goessl@outlook.com>
2022-09-16 21:30:16 +0200

    update code and comment

--- shifter/barrle_shifter.vhd
+++ shifter/barrle_shifter.vhd
@@ -1,63 +1,66 @@
+--------------------------------------------------------------
+--------------------BARRLE-SHIFTER IN VHDL--------------------
+--------------------------------------------------------------
 library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.std_logic_arith.all;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEEstd_logic_arith.all;
 
 entity bs_vhdl is
+    port(   --Inputs--
+            datain      : in std_logic_vector(31 downto 0);
+            direction   : in std_logic;
+            rotation    : in std_logic;
+            count       : in std_logic_vector(4 downto 0);
+            --Outputs--
+            dataout     : out std_logic_vector(31 downto 0));
+end entity bs_vhdl;
 
-port(   datain : std_logic_vector(31 downto 0);
-        direction : in std_logic;
-        rotation : in std_logic;
-        count : in std_logic_vector(4 downto 0);
-        dataout : out std_logic_vector(31 downto 0));
-end bs_vhdl;
-
+--Architecture for the barrel shifter--
 architecture behv of bs_vhdl is
 
---SHIFT LEFT/RIGHT FUNCTION
+    --shift LEFT/RIGHT function--
+    function barrel_shift(din : in std_logic_vector(31 downto 0));
+    dir : in std_logic;
+    cnt : in std_logic_vector(4 downto 0) return std_logic_vector is
 
-function barrel_shift(din : in std_logic_vector(31 downto 0);
-dir : in std_logic;
-cnt : in std_logic_vector(4 downto 0)) return std_logic_vector is 
-begin
-    if(dir = '1')then
-        return std_logic_vector((SHR(unsigned(din), unsigned(cnt))));
-        else
-        return std_logic_vector((SHL(unsigned(din), unsigned(cnt))));
-    end if;
-end barrel_shift;
+        begin
+            if(dir = '1') then
+                return std_logic_vector((SHR(unsigned(din), unsigned(cnt))));
+            else
+                return std_logic_vector((SHL(unsigned(din), unsigned(cnt))));
+            end if;
 
---ROTATE LEFT/RIGHT FUNCTION
+    end barrel_shift;
 
-function barrel_rotate(din : in std_logic_vector(31 downto 0);
-dir : in std_logic;
-cnt : in std_logic_vector(4 downto 0)) return std_logic_vector is 
-variable temp1, temp2 : std_logic_vector(63 downto 0);
+    --rotate LEFT/RIGHT function--
+    function barrel_rotate(din : in std_logic_vector(31 downto 0));
+    dir : in std_logic;
+    cnt : in std_logic_vector(4 downto 0) return std_logic_vector is
+
+        variable temp1, temp2 : std_logic_vector(63 downto 0);
+
+            begin
+                case dir is
+                    when '1' => --rotate right cnt times--
+                        temp1 := din & din;
+                        temp2 := std_logic_vector(SHR(unsigned(temp1), unsigned(cnt)));
+                        return temp2(31 downto 0);
+                    when others => --rotate left cnt times--
+                        temp1 := din & din;
+                        temp2 := std_logic_vector(SHL(unsigned(temp1), unsigned(cnt)));
+                        return temp2(62 downto 32);
+                end case;
+    end barrel_rotate;
 
     begin
 
-    case dir is
-        when'1' => --rotate right cnt times
-        temp1 := din & din;
-        temp2 := std_logic:vector(SHR(unsigned(temp1), unsigned(cnt)));
-        return temp2(31 downto 0);
-        when others => --rotate left cnt times
-        temp1 := din & din;
-        temp2 := std_logic_vector(SHL(unsigned(temp1), unsigned(cnt)));
-        return temp2(63 downto 32);
-    end case;
-end barrel_rotate;
+        P1:process(datain, direction, rotation, count)
+        begin
+            if(rotation = '0') then --shift only--
+                dataout <= barrel_shift(datain, direction, count);
+            else --rotate--
+                dataout <= barrel_rotate(datain, direction, count);
+            end if;
+        end process;
 
-begin
-
-    P1:process(datain, direction, rotation, count)
-    begin
-        if(rotation = '0')then --shift only
-            dataout<=barrel_shift(datain, direction, count);
-        else --rotate only
-            dataout<=barrel_rotate(datain, direction, count);
-        end if;
-    end process;
-end behv;
- 
-
-
+end architecture behv;
