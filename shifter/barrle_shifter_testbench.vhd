156e65656844186e75adb6696acd32efda4ac170
m3ndax <adrian.goessl@outlook.com>
2022-09-17 17:14:58 +0200

    update code and comment

--- shifter/barrle_shifter_testbench.vhd
+++ shifter/barrle_shifter_testbench.vhd
@@ -1,82 +1,86 @@
+---------------------------------------------------------------------------
+---------------------BARRLE-SHIFTER-TESTBENCH IN VHDL----------------------
+---------------------------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
-use IEE.std_logic_arith.all;
+use IEEE.std_logic_arith.all;
 
+--entity declaration--
 entity tb_bs is
 end tb_bs;
 
+--architecture declaration--
 architecture behv of tb_bs is
 
---Instantiating the UUT
+    --instanting the UUT--
+    component bs_vhdl
+        port(   --Inputs--
+                datain      : in  std_logic_vector(7 downto 0);
+                direction   : in  std_logic;
+                count       : in  std_logic_vector(4 downto 0);
+                --Outputs--
+                dataout     : out std_logic_vector(31 downto 0));
+    end component bs_vhdl;
 
-component bs_vhdl
-    port(datain : in std_logic_vector(31 downto 0);
-        direction : in std_logic;
-        count : in std_logic_vector(4 downto 0);
-        dataout : out std_logic_vector(31 downto 0));
-end component;
+    --defining the signals connected to the UUT--
+    signal datain      : std_logic_vector(31 downto 0);
+    signal direction   : std_logic;
+    signal count       : std_logic_vector(4 downto 0);
+    signal dataout     : std_logic_vector(31 downto 0);
 
---Defining the signals connected to the UUT
+    begin
 
-signal datain : std_logic_vector(31 downto 0);
-signal direction : std_logic;
-signal rotation : std_logic;
-signal count : std_logic_vector(4 downto 0);
-signal dataout : std_logic_vector(31 downto 0);
+        --instanting the UUT--
+        |1 : bs_vhdl port map(
+            datain      => datain,
+            direction   => direction,
+            count       => count,
+            dataout     => dataout
+        );
 
-begin
+        --applying stimulus to the UUT--
+        P1 : process
 
---Instantiating the UUT
+            begin
+                wait for 2 ns;
+                    rotation    <= '0'; --shift mode--
+                    datain      <= To_std_logic_vector(X"AAAAAAAA");
+                    direction   <= '0': --left shift--
 
-|1 : bs_vhdl port map(datain => datain,
-direction => direction,
-rotation => rotation,
-count => count,
-dataout => dataout);
+                    loop1 : for i in 0 to 31 loop
+                        count <= To_std_logic_vector(i, 5);
+                        wait for 10 ns;
+                    end loop;
 
---Applying Stimuli
+                    direction <= '1'; --RIGHT
+                    loop3 : for i in 0 to 31 loop
+                        count <= conv_std_logic_vector(i, 5);
+                    wait for 10 ns;
+                    end loop;
 
-P1 : process
+                    direction <= '1'; --RIGHT
+                    rotation <= '1'; --barrel shift
+                    datain <= To_std_logic_vector(X"55555555");
+                    loop2 : for i in 0 to 31 loop
+                        count <= conv_std_logic_vector(i, 5);
+                    wait for 10 ns;
+                    end loop;
 
-    	begin
-        wait for 2 ns;
-        rotation <= '0'; --shift mode
-        datain <= To_std_logic_vector(X"AAAAAAAA");
-        direction <= '0'; --LEFT
-        loop1 : for i in 0 to 31 loop
-            count <= conv_std_logic_vector(i, 5);
-            wait for 10 ns;
-        end loop;
+                    direction <= '0'; --LEFT
+                    loop4 : for i in 0 to 31 loop
+                        count <= conv_std_logic_vector(i, 5);
+                    wait for 10 ns;
+                    end loop;
+        end process;
 
-        direction <= '1'; --RIGHT
-        loop3 : for i in 0 to 31 loop
-            count <= conv_std_logic_vector(i, 5);
-            wait for 10 ns;
-        end loop;
+end architecture behv;
 
-        direction <= '1'; --RIGHT
-        rotation <= '1'; --barrel shift
-        datain <= To_std_logic_vector(X"55555555");
-        loop2 : for i in 0 to 31 loop
-            count <= conv_std_logic_vector(i, 5);
-            wait for 10 ns;
-        end loop;
-
-        direction <= '0'; --LEFT
-        loop4 : for i in 0 to 31 loop
-            count <= conv_std_logic_vector(i, 5);
-            wait for 10 ns;
-        end loop;
-
-end process;
-end behv;
-
---TOP LEVEL CONFIGURATION DECLERATION
-
+--top level configuration declaration--
 configuration CFG_top of tb_bs is
-    for behb
+    for behv
         for |1 : bs_vhdl use entity work.bs_vhdl(behv);
         end for;
     end for;
-end CFG_top;
+end configuration CFG_top;
 
+                    
