3a2212f3b0a7b202dee6a73c53a40279aa43f071
m3ndax <adrian.goessl@outlook.com>
2022-09-25 11:20:56 +0200

    update code and comment

--- BCD/gray_BCD-vhd.vhd
+++ BCD/gray_BCD-vhd.vhd
@@ -1,13 +1,13 @@
 --------------------------------------------------
-----------------------gray BCD--------------------
+-----------------GRAY-BCD IN VHDL-----------------
 --------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
 
 --entity declaration--
-entity BCD_gray is
+entity BCD_gray is 
     port(   --Inputs--
-            B   : in std_logic_vector(3 downto 0);
+            B   : in  std_logic_vector(3 downto 0);
             --Outputs--
             G   : out std_logic_vector(3 downto 0));
 end entity BCD_gray;
@@ -15,26 +15,29 @@ end entity BCD_gray;
 --architecture declaration--
 architecture BCD_gray_arch of BCD_gray is
 
+    --XOR1 declaration--
     component XOR1 is
         port(   --Inputs--
-                a   : in std_logic;
-                b   : in std_logic;
+                A   : in  std_logic;
+                B   : in  std_logic;
                 --Outputs--
-                c   : out std_logic);
+                C   : out std_logic);
     end component XOR1;
 
+    --buffer declaration--
     component Buffer is
         port(   --Inputs--
-                a   : in std_logic;
+                A   : in  std_logic;
                 --Outputs--
-                b   : out std_logic);
+                B   : out std_logic);
     end component Buffer;
 
-        begin
-
-            B1 : Buffer port map(B(3), G(3));
-            X1 : XOR1 Port map (B(3), B(2), G(2));
-            X2 : XOR1 Port map (B(2), B(1), G(1));
-            X3 : XOR1 Port map (B(1), B(0), G(0));
+    begin
+        --port mapping--
+        B1 : Buffer port map(B(3), G(3));
+        X1 : XOR1 port map(B(3), B(2), G(2));
+        X2 : XOR1 port map(B(2), B(1), G(1));
+        X3 : XOR1 port map(B(1), B(0), G(0));
 
 end architecture BCD_gray_arch;
+
