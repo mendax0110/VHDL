8f04d4a413c193d68c83e393eae56d7a8eeb86e0
m3ndax <adrian.goessl@outlook.com>
2022-09-27 20:09:00 +0200

    update code and comment

--- odd_parity_gen/odd_parity_generator_testbench.vhd
+++ odd_parity_gen/odd_parity_generator_testbench.vhd
@@ -1,42 +1,53 @@
+----------------------------------------------------------------------------
+-------------------ODD-PARITY-GENERATOR-TESTBENCH IN VHDL-------------------
+----------------------------------------------------------------------------
 entity ODD_PARITY_TB is
-end;
+end ODD_PARITY_TB;
 
 library IEEE;
-use IEEE.std_logic_1164.all;
+use IEEE.STD_LOGIC_1164.ALL;
 use WORK.anu.all;
 
+--architecture declaration--
 architecture OP_TB_ARCH of ODD_PARITY_TB is
-    
-    component Parity_Generator1
-        port( input_stream : in input;
+
+  --component declaration--
+  component Parity_Generator1
+      port(   --Inputs--
+              input_stream : in input;
               clk          : in std_logic;
-              parity       : out bit);
-    end component;
+              --Outputs--
+              parity       : out std_logic);
+  end component;
 
-    signal input_stream : input;
-    signal clk          : std_logic;
-    signal parity       : bit;
-    begin
-        U1 : Parity_Generator1
-            port map(   input_stream, CLK
-                        parity => parity);
+  --signal declaration--
+  signal input_stream : input;
+  signal clk          : std_logic;
+  signal parity       : bit;
 
-        Input1 : process (clk)
+  begin
+      U1 : Parity_Generator1
+          port map( input_stream => input_stream,
+                    clk          => clk,
+                    parity       => parity);
+                  
+      --process declaration--
+      Input1 : process(clk)
           begin
-            if clk <= 'U' then clk <= '0' after 1 ns;
-            else clk <= not clk after 1 ns;
-            end if;
-        end process;
+              if clk   <= 'U' then clk <= '0' after 1 ns;
+              else clk <= not clk after 1 ns;
+              end if;
+      end process;
 
-        Input2 : process (input_stream)
+      Input2 : process(clk)
           begin
-            input_stream <= "10100110" after 1 ns, "01111100" after 2 ns;
-        end process;
+              input_stream <= "10100110" after 1 ns, "01111100" after 2 ns;
+      end process;
 
-end OP_TB_ARCH;
+end architecture OP_TB_ARCH;
 
+--configuration declaration--
 configuration cfg_op of ODD_PARITY_TB is
     for OP_TB_ARCH
     end for;
-end cfg_op;
-
+end configuration;
