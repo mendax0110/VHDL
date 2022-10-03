814872eff8f754009b7465d5de20064725cc98c7
m3ndax <adrian.goessl@outlook.com>
2022-10-03 19:59:41 +0200

    update code and comment

--- flop/dflop_sync_reset_enable.vhd
+++ flop/dflop_sync_reset_enable.vhd
@@ -1,38 +1,40 @@
+----------------------------------------------------------------------------------
+-----------------------D-FLOP-SYNCHRON-RESET-ENABLE IN VHDL-----------------------
+----------------------------------------------------------------------------------
 library IEEE;
-use IEEE.std_logic_1164.all;
+use IEEE.STD_LOGIC_1164.ALL;
 
 --entity declaration--
 entity dflop_sync_enable is
     port(   --Inputs--
             clk     : in std_logic;
             reset   : in std_logic;
-            clear_n : in std_logic;
             enable  : in std_logic;
+            clear_n : in std_logic;
             in_1    : in std_logic;
             --Outputs--
-            out_1 : out std_logic);
+            out_1   : out std_logic);
 end entity dflop_sync_enable;
 
 --architecture declaration--
 architecture dflop_sync_enable_arch of dflop_sync_enable is
+
     begin
-        ----------design implementation----------
+        ------------design implementation------------
         Reg_Proc : process(clk, reset)
         begin
-            if (reset = '1') then
+            if(reset = '1') then
                 out_1 <= '0';
             elsif rising_edge(clk) then
 
-                if (enable = '1') then
-
-                    if (clear_n = '0') then
-                        out_1 <= '0';
-                    else
+                if(enable = '1') then
+                    if(clear_n = '1') then
                         out_1 <= in_1;
+                    else
+                        out_1 <= '0';
                     end if;
-                
                 end if;
-
             end if;
-        end process;
+        end process Reg_Proc;
+
 end architecture dflop_sync_enable_arch;
