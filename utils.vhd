6644be5e452b606431d0b38fef93704d6eb11c6d
m3ndax <adrian.goessl@outlook.com>
2022-10-05 18:57:50 +0200

    update code and comment

--- utils.vhd
+++ utils.vhd
@@ -1,28 +1,30 @@
------------------------------------------------------------------------
------------------------------UTILS IN VHDL-----------------------------
------------------------------------------------------------------------
+----------------------------------------------------------------------
+-----------------------------UTILS IN VHDL----------------------------
+----------------------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
 
---entity declarations--
+--package declaration--
 package utils is
 
-    type array_Nbit is array(natural range <>, natural range <>) of std_logic;
+    --type declaration--
+    type array_Nbit is array (natural range <>, natural range <>) of std_logic;
+    --function declaration--
     function to std_logic(L : BOOLEAN) return std_ulogic;
 
 end package utils;
 
---package body declarations--
+--package body declaration--
 package body utils is
 
+    --function body declaration--
     function to_std_logic(L : BOOLEAN) return std_ulogic is
         begin
             if L then
-                return('1');
+                return '1';
             else
-                return('0');
+                return '0';
             end if;
     end function to_std_logic;
 
 end package body utils;
-    
