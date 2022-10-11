20c404fa470449139c2509e2406bd73cc6676a02
m3ndax <adrian.goessl@outlook.com>
2022-10-11 20:08:35 +0200

    update code and comment

--- TFF.vhd
+++ TFF.vhd
@@ -1,14 +1,14 @@
--------------------------------------
--------------T-Flip-Flop-------------
--------------------------------------
+-----------------------------------------
+---------------TFF IN VHDL---------------
+-----------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
 
 --entity declaration--
 entity TFF is
     port(   --Inputs--
-            T   : in std_logic;
-            CLK : in std_logic;
+            T   : in  std_logic;
+            CLK : in  std_logic;
             --Outputs--
             Q   : out std_logic;
             Qb  : out std_logic);
@@ -18,21 +18,20 @@ end entity TFF;
 architecture TFF_arch of TFF is
 
     begin
-        variable tmp: std_logic
+        --variable declaration--
+        variable tmp : std_logic;
+        --process declaration--
         process(CLK)
-
-        begin
-            if (CLK'event and CLK = '1') then
-                if (T = '0') then
-                    tmp := tmp;
-                else if (T = '1') then
-                    tmp := not tmp;
+            begin
+                if(CLK'event and CLK = '1') then
+                    if(T = '0') then
+                        tmp := tmp;
+                    else if(T = '1') then
+                        tmp := not tmp;
+                    end if;
                 end if;
-            end if;
-        
-        Q  <= tmp;
-        Qb <= not tmp;
+            Q  <= tmp;
+            Qb <= not tmp;
         end process;
 
 end architecture TFF_arch;
-            
