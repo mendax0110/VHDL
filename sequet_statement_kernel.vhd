cecc97fe5d5c0374fc662f55f14161462e7fab89
m3ndax <adrian.goessl@outlook.com>
2022-10-16 18:54:40 +0200

    update code and comment

--- sequet_statement_kernel.vhd
+++ sequet_statement_kernel.vhd
@@ -1,22 +1,23 @@
----------------------------------------------------------------------------------
---------------------SEQUENCE-STATEMENT-KERNEL IN VHDL----------------------------
----------------------------------------------------------------------------------
+--------------------------------------------------------------------
+-------------------SEQUENCE-STATEMENT IN VHDL-----------------------
+--------------------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
 
---entity declarations--
+--entity declaration--
 entity dff is
     port(   --Inputs--
-            clock   : in std_logic;
-            din     : in std_logic;
+            clock   : in  std_logic;
+            din     : in  std_logic;
             --Outputs--
             dout    : out std_logic);
 end entity dff;
 
---architecture declarations--
+--architecture declaration--
 architecture dff_arch of dff is
 
     begin
+        --process declaration--
         process(clock)
             begin
                 wait until ((clock'EVENT) and (clock = '1'));
