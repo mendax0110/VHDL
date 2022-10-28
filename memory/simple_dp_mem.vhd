ef73da6d2fe41c4a6cb0ab821fc4823272417b85
m3ndax <adrian.goessl@outlook.com>
2022-10-28 21:20:50 +0200

    update code and comment

--- memory/simple_dp_mem.vhd
+++ memory/simple_dp_mem.vhd
@@ -1,47 +1,47 @@
+----------------------------------------------------------
+-----------------SIMPLE-DP-MEMORY IN VHDL-----------------
+----------------------------------------------------------
 library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 --entity declaration--
 entity simple_dp_mem is
     port(   --Inputs--
-            rst     : in std_logic;
-            clk     : in std_logic;
-            
-            dat_in  : in std_logic_vector(15 downto 0);
-            wr_adr  : in std_logic_vector(9 downto 0);
-            wr_en   : in std_logic;
-            
+            rst     : in  std_logic;
+            clk     : in  std_logic;
+            dat_in  : in  std_logic_vector(15 downto 0);
+            wr_adr  : in  std_logic_vector(9 downto 0);
+            wr_en   : in  std_logic;
+            rd_adr  : in  std_logic_vector(9 downto 0);
             --Outputs--
-            dat_out : out std_logic_vector(15 downto 0);
-            rd_adr  : in std_logic_vector(9 downto 0));
+            dat_out : out std_logic_vector(15 downto 0));
 end entity simple_dp_mem;
 
 --architecture declaration--
 architecture simple_dp_mem_arch of simple_dp_mem is
 
     --2D array type for the RAM--
-    type array_1kx16 is array(1023 downto 0) of 
-                            std_logic_vector(15 downto o);
-    --declare the RAM--
+    type ram_type is array(0 to 1023) of std_logic_vector(15 downto 0);
+
+    --RAM declaration--
     signal ram : array_1kx16;
-    
+
+    --process declaration--
     begin
         process(clk)
         begin
             if rising_edge(clk) then
-
                 if(wr_en = '1') then
                     ram(to_integer(unsigned(wr_adr))) <= dat_in;
-
                     --read-during-write return NEW data--
                     dat_out <= ram(to_integer(unsigned(rd_adr)));
                 else
-                    --read only--
+                    --read data--
                     dat_out <= ram(to_integer(unsigned(rd_adr)));
-
                 end if;
             end if;
         end process;
-                
+
 end architecture simple_dp_mem_arch;
+
