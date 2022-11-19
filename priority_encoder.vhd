a4268f2c24a1e1f0ec61bf058e22515bed5ab9b7
m3ndax <adrian.goessl@outlook.com>
2022-11-19 11:37:10 +0100

    update code and comment

--- priority_encoder.vhd
+++ priority_encoder.vhd
@@ -1,48 +1,48 @@
---------------------------------------------------------------------------------
-----------------------------PRIORITY-ENCODER IN VHDL----------------------------
---------------------------------------------------------------------------------
+--------------------------------------------------------------------------
+-------------------------PRIORITY-ENCODER IN VHDL-------------------------
+--------------------------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
---entity declarations--
-entity priority is
+--entity declaration--
+entity priority_encoder is
     port(   --Inputs--
-            I : in bit_vector(7 downto 0)   --inputs to be prioritised
+            I   : in bit_vector(7 downto 0);  --inputs to be prioritized--
             --Outputs--
-            A : out bit_vector(2 downto 0); --encoded output
-            GS: out bit);                   --group signal output
-end priority;                                      
+            A   : out bit_vector(2 downto 0);  --encoded output--
+            GS  : out bit);                    --gate signal--
+end entity priority_encoder;
 
---architecure declarations--
+--architecture declaration--
 architecture priority_inputs_arch of priority_inputs is
 
-        begin
-                process(I)
-                     begin
-                        GS<='1'; --set default outputs
-                        A<= "000";
-                                if I(7) ='1'then
-                                        A<="111";
-                                elsif I(6) ='1'then
-                                        A<="110";
-                                elsif I(5) ='1'then
-                                        A<="101";
-                                elsif I(4) ='1'then
-                                        A<="100";
-                                elsif I(3) ='1'then
-                                        A<="011";
-                                elsif I(2) ='1'then
-                                        A<="010";
-                                elsif I(1) ='1'then
-                                        A<="001";
-                                elsif I(0) ='1'then
-                                        A<="000";
-                                else
-                                        GS<='0';
-                        end if;
-                end process;
+    begin
+        --process declaration--
+        process(I)
+            begin
+                GS <= '1';  --set default outputs--
+                A <= "000";
+                --check for priority--
+                if I(7) = '1' then
+                    A <= "111";
+                elsif I(6) = '1' then
+                    A <= "110";
+                elsif I(5) = '1' then
+                    A <= "101";
+                elsif I(4) = '1' then
+                    A <= "100";
+                elsif I(3) = '1' then
+                    A <= "011";
+                elsif I(2) = '1' then
+                    A <= "010";
+                elsif I(1) = '1' then
+                    A <= "001";
+                elsif I(0) = '1' then
+                    A <= "000";
+                else
+                    GS <= '0';  --set gate signal to 0 if no input is high--
+                end if;
+        end process;
 
-end  architecture priority_inputs;
-            
-            
+end architecture priority_inputs_arch;
